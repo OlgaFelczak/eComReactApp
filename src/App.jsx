7dfcd57c1a63a41a758d9bb8cc2f2a80078408f2
Olga Felczak <ofelczak@gmail.com>
2023-03-16 18:18:46 +0000

    Deleted unnecessary styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import './App.css'
-import Header from './components/Header'
-import LatestProducts from "./components/LatestProducts"
-import Carousel from "./components/Carousel"
-import Footer from "./components/Footer"
+import React from 'react';
+import Header from './components/Header';
+import LatestProducts from './components/LatestProducts';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
- return (
-    <div className="App">
+  return (
+    <div className='App'>
       <Header />
       <LatestProducts />
-      <Carousel /> 
+      <Carousel />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
