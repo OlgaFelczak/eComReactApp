2f86a702454b5a60569d3cecaa2f1d8b9657c66f
Olga Felczak <ofelczak@gmail.com>
2023-03-16 19:08:12 +0000

    Updated Footer classes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,96 +4,96 @@ import { BsImage, BsFacebook, BsInstagram, BsPinterest } from 'react-icons/bs';
 
 const Footer = () => {
   return (
-    <div class='container'>
-      <footer class='py-5 my-5 border-top'>
-        <div class='row'>
-          <div class='col-2'>
+    <div className='container'>
+      <footer className='py-5 my-5 border-top'>
+        <div className='row'>
+          <div className='col-2'>
             <h5>Logo</h5>
             <a
               href='/'
-              class='d-flex align-items-center mb-3 link-dark text-decoration-none'
+              className='d-flex align-items-center mb-3 link-dark text-decoration-none'
             >
               <BsImage />
             </a>
           </div>
 
-          <div class='col-2'>
+          <div className='col-2'>
             <h5>Section</h5>
-            <ul class='nav flex-column'>
-              <li class='nav-item mb-2'>
-                <a href='#' class='nav-link p-0 text-muted'>
+            <ul className='nav flex-column'>
+              <li className='nav-item mb-2'>
+                <a href='#' className='nav-link p-0 text-muted'>
                   Home
                 </a>
               </li>
-              <li class='nav-item mb-2'>
-                <a href='#' class='nav-link p-0 text-muted'>
+              <li className='nav-item mb-2'>
+                <a href='#' className='nav-link p-0 text-muted'>
                   Features
                 </a>
               </li>
-              <li class='nav-item mb-2'>
-                <a href='#' class='nav-link p-0 text-muted'>
+              <li className='nav-item mb-2'>
+                <a href='#' className='nav-link p-0 text-muted'>
                   Pricing
                 </a>
               </li>
-              <li class='nav-item mb-2'>
-                <a href='#' class='nav-link p-0 text-muted'>
+              <li className='nav-item mb-2'>
+                <a href='#' className='nav-link p-0 text-muted'>
                   FAQs
                 </a>
               </li>
-              <li class='nav-item mb-2'>
-                <a href='#' class='nav-link p-0 text-muted'>
+              <li className='nav-item mb-2'>
+                <a href='#' className='nav-link p-0 text-muted'>
                   About
                 </a>
               </li>
             </ul>
           </div>
 
-          <div class='col-2'>
+          <div className='col-2'>
             <h5>Section</h5>
-            <ul class='nav flex-column'>
-              <li class='nav-item mb-2'>
-                <a href='#' class='nav-link p-0 text-muted'>
+            <ul className='nav flex-column'>
+              <li className='nav-item mb-2'>
+                <a href='#' className='nav-link p-0 text-muted'>
                   Home
                 </a>
               </li>
-              <li class='nav-item mb-2'>
-                <a href='#' class='nav-link p-0 text-muted'>
+              <li className='nav-item mb-2'>
+                <a href='#' className='nav-link p-0 text-muted'>
                   Features
                 </a>
               </li>
-              <li class='nav-item mb-2'>
-                <a href='#' class='nav-link p-0 text-muted'>
+              <li className='nav-item mb-2'>
+                <a href='#' className='nav-link p-0 text-muted'>
                   Pricing
                 </a>
               </li>
-              <li class='nav-item mb-2'>
-                <a href='#' class='nav-link p-0 text-muted'>
+              <li className='nav-item mb-2'>
+                <a href='#' className='nav-link p-0 text-muted'>
                   FAQs
                 </a>
               </li>
-              <li class='nav-item mb-2'>
-                <a href='#' class='nav-link p-0 text-muted'>
+              <li className='nav-item mb-2'>
+                <a href='#' className='nav-link p-0 text-muted'>
                   About
                 </a>
               </li>
             </ul>
           </div>
 
-          <div class='col-4 offset-1'>
+          <div className='col-4 offset-1'>
             <form>
               <h5>Subscribe to our newsletter</h5>
               <p>Monthly digest of whats new and exciting from us.</p>
-              <div class='d-flex w-100 gap-2'>
-                <label for='newsletter1' class='visually-hidden'>
+              <div className='d-flex w-100 gap-2'>
+                <label htmlFor='newsletter1' className='visually-hidden'>
                   Email address
                 </label>
                 <input
                   id='newsletter1'
                   type='text'
-                  class='form-control'
+                  className='form-control'
                   placeholder='Email address'
                 />
-                <button class='btn btn-primary' type='button'>
+                <button className='btn btn-primary' type='button'>
                   Subscribe
                 </button>
               </div>
@@ -101,21 +101,21 @@ const Footer = () => {
           </div>
         </div>
 
-        <div class='d-flex justify-content-between py-4 my-4 border-top'>
+        <div className='d-flex justify-content-between py-4 my-4 border-top'>
           <p>&copy; 2023 Company, Inc. All rights reserved.</p>
-          <ul class='list-unstyled d-flex'>
-            <li class='ms-3'>
-              <a class='link-dark' href='https://www.facebook.com/'>
+          <ul className='list-unstyled d-flex'>
+            <li className='ms-3'>
+              <a className='link-dark' href='https://www.facebook.com/'>
                 <BsFacebook />
               </a>
             </li>
-            <li class='ms-3'>
-              <a class='link-dark' href='https://www.instagram.com/'>
+            <li className='ms-3'>
+              <a className='link-dark' href='https://www.instagram.com/'>
                 <BsInstagram />
               </a>
             </li>
-            <li class='ms-3'>
-              <a class='link-dark' href='https://www.pinterest.com/'>
+            <li className='ms-3'>
+              <a className='link-dark' href='https://www.pinterest.com/'>
                 <BsPinterest />
               </a>
             </li>
