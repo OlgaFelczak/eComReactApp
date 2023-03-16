0f20326725926c8b22c2ffcfcd195456aa53b8f6
Pav85 <pawel.werbowy@gmail.com>
2023-03-16 17:11:05 +0000

    update

--- src/components/LatestProducts.jsx
+++ src/components/LatestProducts.jsx
@@ -14,20 +14,20 @@ const LatestProducts = (props) => {
         image={products[0].image}
         title={products[0].title}
         description={products[0].description}
-        price={products[0].description}
+        price={products[0].price}
         />
         </div>
         <div className='col-sm'><ProductCard
         image={products[1].image}
         title={products[1].title}
         description={products[1].description}
-        price={products[1].description}
+        price={products[1].price}
         /></div>
         <div className='col-sm'><ProductCard
         image={products[2].image}
         title={products[2].title}
         description={products[2].description}
-        price={products[2].description}
+        price={products[2].price}
         /></div>
       </div>
     </div>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -14,11 +14,12 @@ import Card from 'react-bootstrap/Card';
 function ProductCard(props) {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={props.image} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>{props.description}</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Text>{props.price}</Card.Text>
+        <Button variant="primary">Buy</Button>
       </Card.Body>
     </Card>       
   );
