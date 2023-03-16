613199d6aaa4fd1f0090de0a1eabd7bebeebd67c
Pav85 <pawel.werbowy@gmail.com>
2023-03-16 14:37:18 +0000

    latest products

--- src/components/LatestProducts.jsx
+++ src/components/LatestProducts.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import ProductCard from './ProductCard'
+import products from "../products.json"
 
 const LatestProducts = () => {
   return (
     <div className='container'>
       <h2>Latest Products</h2>
+      <div className='row'>
+        <div className='col-sm'><ProductCard
+        image={products[0].image}
+        title={products[0].title}
+        description={products[0].description}
+        price={products[0].description}
+        /></div>
+        <div className='col-sm'><ProductCard /></div>
+        <div className='col-sm'><ProductCard /></div>
+      </div>
     </div>
   )
 }
