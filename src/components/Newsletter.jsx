e5fa30091e6a80aa1a19431d9e7817bff6183a9f
Olga Felczak <ofelczak@gmail.com>
2023-03-17 17:02:38 +0000

    Added validation for email input using React Hook Form

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
-function Newsletter() {
+const Newsletter = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div className='col-4 offset-1'>
-      <form>
-        <h5>Sign up to our newsletter!</h5>
-        <p>
-          Millions already enjoy our free emails full of offers & new products.
-        </p>
-        <div className='d-flex w-100 gap-2'>
-          <label htmlFor='newsletter1' className='visually-hidden'>
-            Email address
-          </label>
-          <input
-            id='newsletter1'
-            type='text'
-            className='form-control'
-            placeholder='Email address'
-          />
-          <button className='btn btn-primary' type='button'>
-            Subscribe
-          </button>
-        </div>
-      </form>
-    </div>
+    <form className='col-4 offset-1' onSubmit={handleSubmit(onSubmit)}>
+      <h5>Sign up to our newsletter!</h5>
+      <p>
+        Millions already enjoy our free emails full of offers & new products.
+      </p>
+      <div className='d-flex w-100 gap-2'>
+        <label htmlFor='newsletter1' className='visually-hidden'>
+          Email address
+        </label>
+        <input
+          {...register('email', {
+            required: true,
+            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+          })}
+          type='text'
+          className='form-control'
+          placeholder='Email address'
+        />
+        {errors?.email?.type === 'required' && <span>Email is required</span>}
+        {errors?.email?.type === 'pattern' && (
+          <span style={{ color: 'red' }}>This is not valid email</span>
+        )}
+        <input type='submit' className='btn btn-primary' val='Subscribe' />
+      </div>
+    </form>
   );
-}
+};
 
 export default Newsletter;
