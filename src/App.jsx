7d87454242ef3eef7f36e1781fbb06a5d3e6198a
Olga Felczak <ofelczak@gmail.com>
2023-03-18 16:12:08 +0000

    react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Product from './pages/Product';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: 'Product',
+    element: <Product />,
+  },
+]);
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <Home />
+      <RouterProvider router={router} />
       <Footer />
     </div>
   );
---
+++ src/pages/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import products from '../products.json';
+
+const Product = () => {
+  return (
+    <div className='container'>
+      Product page
+    </div>
+  );
+};
+
+export default Product;
