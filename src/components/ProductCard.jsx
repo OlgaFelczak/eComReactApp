83a1ba4444f922261789c5725d6c563c13a01e18
Pav85 <pawel.werbowy@gmail.com>
2023-03-18 23:07:14 +0000

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,9 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+
 import "../styles/productCard.css";
 import { CartContext } from './CartContext';
 import { useContext } from 'react';
@@ -8,7 +12,7 @@ function ProductCard(props) {
   const product = props.product; 
   const cart = useContext(CartContext);
   const productQuantity = cart.getProductQuantity(product.id);
-  console.log(cart.items);
+  // console.log(cart.items);
  
   return (
     <Card className='card'>
@@ -17,7 +21,22 @@ function ProductCard(props) {
         <Card.Title>{product.title}</Card.Title>
         <Card.Text>{product.description}</Card.Text>
         <Card.Text>{product.price}</Card.Text>
+        { productQuantity > 0 ?
+        
+        <>
+        <Form as={Row}>
+        <Form.Label column="true" sm="6">In Cart: {productQuantity}</Form.Label>
+        <Col sm="6">
+          <Button sm="6" className='mx-2' onClick={() => cart.addOneToCart(product.id)}>+</Button>
+          <Button sm="6" className='mx-2' onClick={() => cart.removeOneFromCart(product.id)}>-</Button>
+        </Col>
+        </Form>
+        <Button variant='danger' className='my-2' onClick={() => cart.deleteFromCart(product.id)}>Remove from Cart</Button>
+        </> 
+        :
         <Button variant="primary" onClick={() => cart.addOneToCart(product.id)}>Add to Cart</Button>
+         }
+        
       </Card.Body>
     </Card>   
     //  <Card className='card'>
@@ -30,6 +49,5 @@ function ProductCard(props) {
     //   </Card.Body>
     // </Card>      
   );
-}
-
+        }
 export default ProductCard;
