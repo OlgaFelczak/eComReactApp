3c7c65c124fc0b0e248dc78027258494eba58998
Pav85 <pawel.werbowy@gmail.com>
2023-03-20 14:05:28 +0000

    clear local storage

--- src/pages/Cancel.jsx
+++ src/pages/Cancel.jsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
 
 function Cancel() {
+
+   useEffect(() => {
+      JSON.parse(
+        !window.localStorage.clear('products')
+      );
+  
+
+    });
+
    return (
     <h1>Sorry to see you cancelled your order.</h1>
    )
