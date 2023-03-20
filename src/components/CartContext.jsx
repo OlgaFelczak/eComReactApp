8a203293ad486d075457480cef132e0ef5d1cf00
Olga Felczak <ofelczak@gmail.com>
2023-03-20 12:42:29 +0000

    Added LocalStorage

--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -1,103 +1,111 @@
-
-import { createContext, useState } from "react";
-import { productsArray, getProductData } from "../productsStore";
+import { createContext, useState, useEffect } from 'react';
+import { getProductData } from '../productsStore';
 
 export const CartContext = createContext({
-    items: [],
-    getProductQuantity: () => {},
-    addOneToCart: () => {},
-    removeOneFromCart: () => {},
-    deleteFromCart: () => {},
-    getTotalCost: () => {},
-})
+  items: [],
+  getProductQuantity: () => {},
+  addOneToCart: () => {},
+  removeOneFromCart: () => {},
+  deleteFromCart: () => {},
+  getTotalCost: () => {},
+});
 
-export function CartProvider({children}) {
-    const [cartProducts, setCartProducts] = useState([]);
+export function CartProvider({ children }) {
+  const [cartProducts, setCartProducts] = useState([]);
 
-    function getProductQuantity(id) {
-        const quantity = cartProducts.find(product => product.id === id)?.quantity;
+  // Add data to localStorage
+  useEffect(() => {
+    const localStorageProducts = JSON.parse(
+      window.localStorage.getItem('products')
+    );
 
-        if (quantity === undefined) {
-            return 0;
+    if (localStorageProducts) setCartProducts(localStorageProducts);
+  }, []);
+
+  useEffect(() => {
+    if (cartProducts && cartProducts.length !== 0)
+      window.localStorage.setItem('products', JSON.stringify(cartProducts));
+  }, [cartProducts]);
+
+  function getProductQuantity(id) {
+    const quantity = cartProducts.find(
+      (product) => product.id === id
+    )?.quantity;
+
+    if (quantity === undefined) {
+      return 0;
     }
     return quantity;
-}
+  }
 
-    function addOneToCart(id) {
-        const quantity = getProductQuantity(id);
+  function addOneToCart(id) {
+    const quantity = getProductQuantity(id);
 
-        if (quantity === 0) {
-            setCartProducts([
-                ...cartProducts,
-                {
-                    id: id,
-                    quantity: 1
-                }
-            ])
-        } else {
-            setCartProducts(
-                cartProducts.map(
-                    product =>
-                    product.id === id
-                    ? { ...product, quantity: product.quantity + 1}
-                    : product
-                )
-            )
-        }
-    }
-
-    function deleteFromCart(id) {
-        setCartProducts(
-            cartProducts => 
-            cartProducts.filter(currentProduct => {
-                return currentProduct.id != id
-            })      
+    if (quantity === 0) {
+      setCartProducts([
+        ...cartProducts,
+        {
+          id: id,
+          quantity: 1,
+        },
+      ]);
+    } else {
+      setCartProducts(
+        cartProducts.map((product) =>
+          product.id === id
+            ? { ...product, quantity: product.quantity + 1 }
+            : product
         )
+      );
     }
+  }
 
-    function removeOneFromCart(id) {
-        const quantity = getProductQuantity(id);
+  function deleteFromCart(id) {
+    setCartProducts((cartProducts) =>
+      cartProducts.filter((currentProduct) => {
+        return currentProduct.id != id;
+      })
+    );
+  }
 
-        if (quantity == 1) {
-            deleteFromCart(id); 
-        } else {
-            setCartProducts(
-                cartProducts.map(
-                    product =>
-                    product.id === id
-                    ? { ...product, quantity: product.quantity - 1}
-                    : product
-                )
-            )
-        }
+  function removeOneFromCart(id) {
+    const quantity = getProductQuantity(id);
+
+    if (quantity == 1) {
+      deleteFromCart(id);
+    } else {
+      setCartProducts(
+        cartProducts.map((product) =>
+          product.id === id
+            ? { ...product, quantity: product.quantity - 1 }
+            : product
+        )
+      );
     }
+  }
 
-    function getTotalCost() {
-        let totalCost = 0;
-        cartProducts.map((cartItem) => {
-            const productData = getProductData(cartItem.id);
-            totalCost += (productData.price * cartItem.quantity);
-        });
+  function getTotalCost() {
+    let totalCost = 0;
+    cartProducts.map((cartItem) => {
+      const productData = getProductData(cartItem.id);
+      totalCost += productData.price * cartItem.quantity;
+    });
 
-        return totalCost;
-    }
+    return totalCost;
+  }
 
-const contextValue = {
+  const contextValue = {
     items: cartProducts,
     getProductQuantity,
     addOneToCart,
     removeOneFromCart,
     deleteFromCart,
     getTotalCost,
-}
+  };
 
-return (
-    <CartContext.Provider value={contextValue}>
-        {children}
-    </CartContext.Provider>
-)
-
-
+  return (
+    <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
+  );
 }
 
 export default CartProvider;
