abb4e8a054e19f73774c4402b8d8ced55740aeab
Olga Felczak <ofelczak@gmail.com>
2023-03-20 17:31:33 +0000

    Linked image from Store page to Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,40 +1,30 @@
 import React from 'react';
 import { useLoaderData } from 'react-router';
-import products from '../products.json';
 import '../styles/product.css';
-import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export async function loader({ params }) {
-  return products.find((value) => value.id === parseInt(params.productId));
+  const { data: products } = await axios.get(
+    'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline&product_type='
+  );
+  return products.find((product) => product.id === parseInt(params.productId));
 }
 
 const Product = () => {
   const product = useLoaderData();
 
-  const [apiProducts, setApiProducts] = useState([]);
-
-  const fetchProducts = async () => {
-      const responseApiProducts = await axios.get('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline&product_type=');
-      setApiProducts(responseApiProducts.data);
-  }
-
-  useEffect(() => {
-      fetchProducts();
-  }, [])
-
   return (
     <div className='container'>
       <div className='row'>
-        {/* <div className='col-6'>
+        <div className='col-6'>
           <img
             className='product-img'
-            src={product.image}
-            alt={product.imageAlt}
+            src={product.image_link}
+            alt={product.name}
           />
         </div>
         <div className='col-6'>
-          <h3>{product.title}</h3>
+          <h3>{product.name}</h3>
           <p>{product.description}</p>
           <div className='row'>
             <div className='col-6'>
@@ -46,21 +36,7 @@ const Product = () => {
               </button>
             </div>
           </div>
-        </div> */}
-                    {
-                        apiProducts.map(productsApi => {
-                            return (
-                                <ProductCard
-                                    id={productsApi.id}
-                                    key={productsApi.id}
-                                    name={productsApi.brand}
-                                    src={productsApi.image_link}
-                                    price={productsApi.price}
-                                    description={productsApi.description}
-                                />
-                            )
-                        })
-                    }
+        </div>
       </div>
     </div>
   );
